c2589dc5c037b1b5b7e86bc32530f485726e94a7
andi fauzi <andifauzi0709@gmail.com>
2025-02-20 23:57:12 +0800

    fix: tailwind css variabel issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import './styles/app.css';
 
 // Set up a Router instance
 const router = createRouter({
-    routeTree,
-    defaultPreload: "intent",
+	routeTree,
+	defaultPreload: 'intent',
 });
 
 // Register things for typesafety
-declare module "@tanstack/react-router" {
-    interface Register {
-        router: typeof router;
-    }
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router;
+	}
 }
 
-const rootElement = document.getElementById("app")!;
+const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    );
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	);
 }
