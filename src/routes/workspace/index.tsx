45d17823ef8358ffd7c0b6ea005fee2d6d0b3ddf
andi fauzi <andifauzi0709@gmail.com>
2025-03-03 17:29:32 +0800

    feature: Add /workspaces pages & consume workspace hooks on the routes

--- src/routes/workspace/index.tsx
+++ src/routes/workspace/index.tsx
@@ -1,9 +1,71 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
+import { TitleWrapper } from '@/components/wrapperElement';
+import { useUserData } from '@/hooks/useUserData';
+import { useWorkspace } from '@/hooks/useWorkspace';
+import { createFileRoute } from '@tanstack/react-router';
+import { ColumnDef } from '@tanstack/react-table';
 
 export const Route = createFileRoute('/workspace/')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
+
+type Role = 'Manager' | 'Admin' | 'Staff';
+
+interface Workspaces {
+	userid: string;
+	email: string;
+	status: string;
+	role: Role;
+}
 
 function RouteComponent() {
-  return <div>Hello "/workspace/"!</div>
+	const { workspaceid } = useUserData();
+	const { selectedWorkspace, updateWorkspace, loading, error } = useWorkspace(Number(workspaceid));
+
+	const usersColumn: ColumnDef<Workspaces>[] = [
+		{
+			accessorKey: 'userid',
+			header: () => <h1 className="pl-8">User ID</h1>,
+			cell: ({ row }) => <h1 className="pl-8">{row.original.userid}</h1>,
+		},
+		{
+			accessorKey: 'email',
+			header: 'Email',
+		},
+		{
+			accessorKey: 'status',
+			header: 'Status',
+		},
+		{
+			accessorKey: 'role',
+			header: 'Role',
+		},
+		{
+			id: 'actions',
+			header: '',
+			cell: () => (
+				<div className="w-full flex justify-end">
+					<Button
+						variant="outline"
+						className="w-20 text-xs border-t-0 border-r-0 border-b-0">
+						REMOVE
+					</Button>
+				</div>
+			),
+		},
+	];
+
+	return (
+		<>
+			<TitleWrapper className="border-b-0">
+				<h1>Users</h1>
+			</TitleWrapper>
+			<DataTable
+				columns={usersColumn}
+				data={selectedWorkspace?.users || []}
+				loading={loading}
+			/>
+		</>
+	);
 }
