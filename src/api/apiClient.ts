33feeca72e0213c44346c45917f73f1106e85aa4
h7mei <hanafichoirulloh1@gmail.com>
2025-03-22 18:48:35 +0700

    base url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = 'https://oscloud-backend.vercel.app/v1';
-const API_BASE_URL = "http://localhost:8000/v1";
+const API_BASE_URL = "https://oscloud-backend.vercel.app/v1";
+// const API_BASE_URL = "http://localhost:8000/v1";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
