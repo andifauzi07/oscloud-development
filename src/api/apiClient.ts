080282ed29c053367d3d1fa572261e3658f1d565
andi fauzi <andifauzi0709@gmail.com>
2025-03-27 16:29:27 +0800

    minor-fix: move apiUrl to env file

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE_URL = "https://oscloud-backend.vercel.app/v1";
-// const API_BASE_URL = "http://localhost:8000/v1";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL!;
 
 const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
+	baseURL: API_BASE_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	withCredentials: true,
 });
 
 export default apiClient;
